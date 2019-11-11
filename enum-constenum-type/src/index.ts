a1d0fd59977a07742f1bba545dc6b2b910da13da
tiagonapoli <tiago.napoli@vtex.com.br>
2019-11-11 14:52:44 -0300

    Enum - const enum - type

---
+++ enum-constenum-type/src/index.ts
@@ -0,0 +1,19 @@
+import { ProductSizes1, ProductSizes2, ProductSizes3 } from './types'
+
+const fn1 = (productSize: ProductSizes1) => {
+    if(productSize === 'BIG') {
+
+    }
+}
+
+const fn2 = (productSize: ProductSizes2) => {
+    if(productSize === ProductSizes2.BIG) {
+
+    }
+}
+
+const fn3 = (productSize: ProductSizes3) => {
+    if(productSize === ProductSizes3.BIG) {
+        
+    }
+}
---
+++ enum-constenum-type/src/types.ts
@@ -0,0 +1,11 @@
+export type ProductSizes1 = 'BIG' | 'SMALL'
+
+export enum ProductSizes2 {
+    BIG = "BIG",
+    SMALL = "SMALL"
+}
+
+export const enum ProductSizes3 {
+    BIG = "BIG",
+    SMALL = "SMALL"
+}
